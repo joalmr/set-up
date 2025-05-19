8d5bd3420ce336a6f45a3ca7e5d87c38a4fca0a3
Alonso Massa <alonso@MacBook-Pro-de-Alonso.local>
2025-05-19 12:08:52 -0500

    .

--- t_arch.sh
+++ t_arch.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
 ./t_core.sh
 ./t_feature.sh
+
+# Agrega el paquete provider al proyecto Flutter
+flutter pub add provider
+# Agrega el paquete go_router al proyecto Flutter
+flutter pub add go_router
