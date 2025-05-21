4efd97dc93b83d8b40c3f49cc6b150dddde6d807
Alonso Massa <alonso@MacBook-Pro-de-Alonso.local>
2025-05-21 17:16:32 -0500

    env

--- t_arch.sh
+++ t_arch.sh
@@ -7,3 +7,24 @@
 flutter pub add provider
 # Agrega el paquete go_router al proyecto Flutter
 flutter pub add go_router
+# Agrega el paquete intl al proyecto Flutter
+flutter pub add intl
+# Agrega el paquete equatable al proyecto Flutter
+flutter pub add equatable
+# Agrega el paquete logger al proyecto Flutter
+flutter pub add logger
+
+# Función para crear archivos Dart con contenido básico
+crear_archivo_dart() {
+  local ruta_archivo=$1
+  local contenido=$2
+
+  # Crear el archivo y añadir el contenido
+  echo "$contenido" > "$ruta_archivo"
+  echo "Creado: $ruta_archivo"
+}
+
+# Crear archivos env para producción y desarrollo
+mkdir -p /
+crear_archivo_dart ".env" '''# PRODUCTION'''
+crear_archivo_dart ".env.dev" '''# DEVELOPMENT'''
